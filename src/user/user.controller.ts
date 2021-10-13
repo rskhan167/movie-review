ed522eada6d845fd0b55ddf8220f1bcb7d461555
rskhan167 <rs.khan167@gmail.com>
2021-10-13 21:39:16 +0530

    add plugin config and apiResponse

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserService } from './user.service';
@@ -9,16 +10,19 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('all')
+  @ApiOkResponse({ status: 200, type: User, isArray: true })
   async getAll(): Promise<User[]> {
     return this.userService.getAll();
   }
 
   @Get(':userId')
+  @ApiOkResponse({ status: 200, type: User })
   async getUser(@Param('userId') userId: number): Promise<User> {
     return await this.userService.getUser(userId);
   }
 
   @Post()
+  @ApiOkResponse({ status: 201, type: User })
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.userService.create(createUserDto);
   }
